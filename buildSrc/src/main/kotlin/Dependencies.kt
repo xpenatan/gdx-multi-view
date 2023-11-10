aed2cd89fd9b456bdca0f24e67845865acfd4e89
Natan <xpenatan@gmail.com>
2023-11-10 08:34:48 -0300

    update wrapper and add buildsrc

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,15 @@
+object LibExt {
+    const val groupId = "com.github.xpenatan"
+    val libVersion: String = getVersion("1.0.0", "b1")
+    const val gdxVersion = "1.12.1"
+}
+
+private fun getVersion(releaseVersion: String, suffix: String = ""): String {
+    val isRelease = System.getenv("RELEASE")
+    var libVersion = "${releaseVersion}-SNAPSHOT"
+    if(isRelease != null && isRelease.toBoolean()) {
+        libVersion = releaseVersion + if(suffix.isNotEmpty()) "-${suffix}" else ""
+    }
+    println("gdx-multi-view Version: $libVersion")
+    return libVersion
+}
