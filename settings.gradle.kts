96fc6f65712832408ae5870092cf2f13f69307a7
Natan <xpenatan@gmail.com>
2024-04-18 08:34:12 -0300

    add debug code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,12 @@ include(":examples:basic:core")
 include(":examples:basic:desktop")
 include(":examples:imgui-window:core")
 include(":examples:imgui-window:desktop")
+
+//includeBuild("E:\\Dev\\Projects\\java\\gdx-imgui") {
+//    dependencySubstitution {
+//        substitute(module("com.github.xpenatan.gdx-imgui:imgui-ext-core")).using(project(":imgui-ext:ext-core"))
+//        substitute(module("com.github.xpenatan.gdx-imgui:imgui-ext-desktop")).using(project(":imgui-ext:ext-desktop"))
+//        substitute(module("com.github.xpenatan.gdx-imgui:imgui-ext-teavm")).using(project(":imgui-ext:ext-teavm"))
+//        substitute(module("com.github.xpenatan.gdx-imgui:gdx-impl")).using(project(":gdx:gdx-impl"))
+//    }
+//}
